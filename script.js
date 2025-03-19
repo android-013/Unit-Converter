fed4f557630c31495cec1386e7543d71da90e3f4
android-013 <azrifat013@gmail.com>
2025-03-19 22:35:06 +0600

    r j7

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function showCategory(category) {
-    const content = document.getElementById('content');
-    content.innerHTML = `<h2>${category.charAt(0).toUpperCase() + category.slice(1)} Conversion</h2>
-                         <p>Conversion options for ${category} will go here.</p>`;
-}
 
 
 
